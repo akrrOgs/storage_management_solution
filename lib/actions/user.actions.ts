1056ea2948d2efa5ccc6a0a44d4675deebcfaee1
akrrOgs <dlordrog@gmail.com>
2024-11-24 20:22:57 +0700

    fix deployment

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -94,19 +94,23 @@ export const verifySecret = async ({
 };
 
 export const getCurrentUser = async () => {
-  const { account, databases } = await createSessionClient();
+  try {
+    const { account, databases } = await createSessionClient();
 
-  const result = await account.get();
+    const result = await account.get();
 
-  const user = await databases.listDocuments(
-    appwriteConfig.database,
-    appwriteConfig.usersCollection,
-    [Query.equal("accountId", result.$id)]
-  );
+    const user = await databases.listDocuments(
+      appwriteConfig.database,
+      appwriteConfig.usersCollection,
+      [Query.equal("accountId", result.$id)]
+    );
 
-  if (user.total <= 0) return null;
+    if (user.total <= 0) return null;
 
-  return parseStringify(user.documents[0]);
+    return parseStringify(user.documents[0]);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const signOutUser = async () => {
